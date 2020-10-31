68253a52c0ffd190e5f4e720878d0e51db1a4918
unknown <Samin Sharar>
2020-10-31 21:03:46 +0600

    Detail Page Design Done and intergrated with home page

--- src/components/DetailContainer/DetailContainer.jsx
+++ src/components/DetailContainer/DetailContainer.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import SectionDirectory from '../SectionDirectory/SectionDirectory';
 
 const DetailContainer = ({detail}) => {
+    if(detail.known_for_department) return(   <div>
+        <h1 style={{fontSize:'50px'}}>{detail.name}</h1>
+    <h2 style={{fontSize:'35px'}}> Birth Date: {detail.birthday ? detail.birthday : ''}</h2>
+        <h3 style={{fontSize:'35px'}}>Occupation:  {detail.known_for_department}</h3>
+    <p style={{fontSize:'30px'}}>Popularity:{detail.popularity} Place of Birth: {detail.place_of_birth} {detail.deathday===null ? '':`Death: ${detail.deathday}`}</p>
+             </div>) 
     return (
         <div>
         <h1 style={{fontSize:'50px'}}>{detail.title}</h1>
--- src/pages/details/DetailPage.jsx
+++ src/pages/details/DetailPage.jsx
@@ -10,13 +10,15 @@ const DetailPage = ({getDetails,match,detail,loading}) => {
         getDetails(match.params.media,match.params.id);
      
     }, [getDetails,match.params.media, match.params.id])
+    const selectedImage = detail.profile_path ? detail.profile_path : detail.poster_path;
+
     if(loading) return<div>Loading....</div>
     return (
         <div>
         <SearchBar />
         <div className='backgroundClass'>
      <div className='main-detail'> 
-        <img src={detail.poster_path===null ? 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/1200px-No_image_3x4.svg.png' :`http://image.tmdb.org/t/p/w220_and_h330_face/${detail.poster_path}`} alt={detail.title} className='center'/>
+        <img src={detail.poster_path === null ? 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/1200px-No_image_3x4.svg.png' :`http://image.tmdb.org/t/p/w220_and_h330_face/${selectedImage}`} alt={detail.title} className='center'/>
         <DetailContainer detail={detail}/>
      </div>
    </div>
